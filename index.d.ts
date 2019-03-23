91ee8aed51aa43ef1ec4454f73122afc67e51ee7
Vincent Briglia <20100.is@gmail.com>
2019-03-23 12:36:53 +0100

    Create index.d.ts
    typings for TS

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export declare const sortOrder: string[];
+export declare function sortPackageJson<T>(packageJson: T, options?: {
+    sortOrder: string[];
+} | undefined): T;
+export default sortPackageJson;
