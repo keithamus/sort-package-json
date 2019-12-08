a91a766939b4f0b65f88511aeb8d6f1f7e54632b
fisker Cheung <lionkay@gmail.com>
2019-12-08 08:25:13 +0800

    refactor: major code refactor (#90)
    * refactor: sortSubKey logic
    
    * refactor: sortOrder
    
    * chore: add comment
    
    * fix: restore key
    
    * fix: add sortList
    
    * build: install prettier, eslint
    
    * style: eslint --fix
    
    * refactor: hoise arrayUnique out of main function
    
    * refactor: hoist parseJSON logic
    
    * refactor: hoist compareScriptKeys
    
    * refactor: omit sortSubKey when falsey
    
    * refactor: skip calling sortSubKey when not needed
    
    * refactor: hoist sortSubKey
    
    * refactor: hoist sortKeys/uniqueKeys
    
    * refactor: make sort/uniq pure
    
    * refactor: hoist custom sortScripts function
    
    * refactor: provide sortScripts as custom comparator
    
    * refactor: hoist stringifyJSON
    
    * refactor: pass sortSubKey as function to options
    
    * refactor: curry options into sortSubKey
    
    * refactor: remove useless if
    
    * refactor: rename sortSubKey option as over
    
    * refactor: make over pure
    
    * refactor: only pass field to over, not whole object
    
    * refactor: split sortSubKey into sortArray/sortObject
    
    * refactor: only call over if key is present
    
    * fix(keywords): do not sort, only uniq. Fixes #83
    
    * refactor: collapse sortArray
    
    * refactor: never mutate original json
    
    * refactor: curry & reverse args for sortObjectKeys
    
    * refactor: hoist onObject
    
    * refactor: separate sortScripts
    
    * refactor: shrink sortObject
    
    * refactor: split sortObject/sortObjectBy
    
    * refactor: hoist reused over functions
    
    * refactor: hoist defaultNpmScripts
    
    * test: remove private from sort test
    
    * test: homepage should not be sorted
    
    * test: do not attempt to sort objects like arrays
    
    * test: sort betterscripts properly
    
    * ci: run lint on ci
    
    * feat: uniq `files` field
    
    * fix: add `onArray` to avoid crash on wrong type of `keywords` and `files`
    
    * fix: add `isPlainObject` type check to avoid sort array accidentally
    
    * refactor: simplify JSON parse/stringify step
    
    * fix: add missing return
    
    * fix: lint errors
    
    * style: simplify `editStringJSON` function
    
    * fix: make prexiableScripts copy defaultNpmScripts
    
    Co-Authored-By: fisker <lionkay@gmail.com>

--- index.js
+++ index.js
@@ -1,78 +1,20 @@
 #!/usr/bin/env node
-const sortObjectKeys = require('sort-object-keys');
-const detectIndent = require('detect-indent');
-const glob = require('glob');
+const _sortObjectKeys = require('sort-object-keys')
+const detectIndent = require('detect-indent')
+const glob = require('glob')
+const sortObjectKeys = comp => x => _sortObjectKeys(x, comp)
 
-const sortOrder = [
-  'name',
-  'version',
-  'private',
-  'description',
-  'keywords',
-  'homepage',
-  'bugs',
-  'repository',
-  'funding',
-  'license',
-  'author',
-  'contributors',
-  'files',
-  'sideEffects',
-  'type',
-  'exports',
-  'main',
-  'umd:main',
-  'jsdelivr',
-  'unpkg',
-  'module',
-  'source',
-  'jsnext:main',
-  'browser',
-  'types',
-  'typings',
-  'style',
-  'example',
-  'examplestyle',
-  'assets',
-  'bin',
-  'man',
-  'directories',
-  'workspaces',
-  'scripts',
-  'betterScripts',
-  'husky',
-  'pre-commit',
-  'commitlint',
-  'lint-staged',
-  'config',
-  'nodemonConfig',
-  'browserify',
-  'babel',
-  'browserslist',
-  'xo',
-  'prettier',
-  'eslintConfig',
-  'eslintIgnore',
-  'stylelint',
-  'ava',
-  'jest',
-  'mocha',
-  'nyc',
-  'dependencies',
-  'devDependencies',
-  'peerDependencies',
-  'bundledDependencies',
-  'bundleDependencies',
-  'optionalDependencies',
-  'flat',
-  'resolutions',
-  'engines',
-  'engineStrict',
-  'os',
-  'cpu',
-  'preferGlobal',
-  'publishConfig'
-];
+const onArray = fn => x => (Array.isArray(x) ? fn(x) : x)
+const uniq = onArray(xs => xs.filter((x, i) => i === xs.indexOf(x)))
+const isPlainObject = x =>
+  x && Object.prototype.toString.call(x) === '[object Object]'
+const onObject = fn => x => (isPlainObject(x) ? fn(x) : x)
+const sortObjectBy = comparator => onObject(sortObjectKeys(comparator))
+const sortObject = onObject(sortObjectKeys())
+const sortURLObject = sortObjectBy(['type', 'url'])
+const sortAuthorObject = sortObjectBy(['name', 'email', 'url'])
+const sortDirectories = sortObjectBy(['lib', 'bin', 'man', 'doc', 'example'])
+
 // See https://docs.npmjs.com/misc/scripts
 const defaultNpmScripts = [
   'install',
@@ -85,183 +27,209 @@ const defaultNpmScripts = [
   'stop',
   'test',
   'uninstall',
-  'version'
-];
+  'version',
+]
 
-function sortPackageJson(packageJson, options = {}) {
-  const determinedSortOrder = options.sortOrder || sortOrder;
-  let wasString = false;
-  let hasWindowsNewlines = false;
-  let endCharacters = '';
-  let indentLevel = 2;
-  if (typeof packageJson === 'string') {
-    wasString = true;
-    indentLevel = detectIndent(packageJson).indent;
-    if (packageJson.substr(-1) === '\n') {
-      endCharacters = '\n';
+const sortScripts = scripts => {
+  const prefixableScripts = defaultNpmScripts.slice()
+  Object.keys(scripts).forEach(script => {
+    const prefixOmitted = script.replace(prefixedScriptRegex, '$2')
+    if (scripts[prefixOmitted] && !prefixableScripts.includes(prefixOmitted)) {
+      prefixableScripts.push(prefixOmitted)
     }
-    const newlineMatch = packageJson.match(/(\r?\n)/);
-    hasWindowsNewlines = (newlineMatch && newlineMatch[0]) === '\r\n';
-    packageJson = JSON.parse(packageJson);
+  })
+  return sortObjectKeys(compareScriptKeys(toSortKey(prefixableScripts)))(
+    scripts,
+  )
+}
+
+// field.key{string}: field name
+// field.over{function}: sort field subKey
+const fields = [
+  { key: 'name' },
+  { key: 'version' },
+  { key: 'private' },
+  { key: 'description' },
+  { key: 'keywords', over: uniq },
+  { key: 'homepage' },
+  { key: 'bugs', over: sortObjectBy(['url', 'email']) },
+  { key: 'repository', over: sortURLObject },
+  { key: 'funding', over: sortURLObject },
+  { key: 'license', over: sortURLObject },
+  { key: 'author', over: sortAuthorObject },
+  { key: 'files', over: uniq },
+  { key: 'sideEffects' },
+  { key: 'type' },
+  { key: 'exports', over: sortObject },
+  { key: 'main' },
+  { key: 'umd:main' },
+  { key: 'jsdelivr' },
+  { key: 'unpkg' },
+  { key: 'module' },
+  { key: 'source' },
+  { key: 'jsnext:main' },
+  { key: 'browser' },
+  { key: 'types' },
+  { key: 'typings' },
+  { key: 'style' },
+  { key: 'example' },
+  { key: 'examplestyle' },
+  { key: 'assets' },
+  { key: 'bin', over: sortObject },
+  { key: 'man', over: sortObject },
+  { key: 'directories', over: sortDirectories },
+  { key: 'workspaces' },
+  { key: 'scripts', over: sortScripts },
+  { key: 'betterScripts', over: sortScripts },
+  { key: 'husky' },
+  { key: 'pre-commit' },
+  { key: 'commitlint', over: sortObject },
+  { key: 'lint-staged', over: sortObject },
+  { key: 'config', over: sortObject },
+  { key: 'nodemonConfig', over: sortObject },
+  { key: 'browserify', over: sortObject },
+  { key: 'babel', over: sortObject },
+  { key: 'browserslist' },
+  { key: 'xo', over: sortObject },
+  { key: 'prettier', over: sortObject },
+  { key: 'eslintConfig', over: sortObject },
+  { key: 'eslintIgnore' },
+  { key: 'stylelint' },
+  { key: 'ava', over: sortObject },
+  { key: 'jest', over: sortObject },
+  { key: 'mocha', over: sortObject },
+  { key: 'nyc', over: sortObject },
+  { key: 'dependencies', over: sortObject },
+  { key: 'devDependencies', over: sortObject },
+  { key: 'peerDependencies', over: sortObject },
+  { key: 'bundledDependencies', over: sortObject },
+  { key: 'bundleDependencies', over: sortObject },
+  { key: 'optionalDependencies', over: sortObject },
+  { key: 'flat' },
+  { key: 'resolutions', over: sortObject },
+  { key: 'engines', over: sortObject },
+  { key: 'engineStrict', over: sortObject },
+  { key: 'os', over: sortObject },
+  { key: 'cpu', over: sortObject },
+  { key: 'preferGlobal', over: sortObject },
+  { key: 'publishConfig', over: sortObject },
+]
+
+const sortOrder = fields.map(({ key }) => key)
+
+function editStringJSON(json, over) {
+  if (typeof json === 'string') {
+    const indentLevel = detectIndent(json).indent
+    const endCharacters = json.slice(-1) === '\n' ? '\n' : ''
+    const newlineMatch = json.match(/(\r?\n)/)
+    const hasWindowsNewlines = (newlineMatch && newlineMatch[0]) === '\r\n'
+    json = JSON.parse(json)
+
+    let result = JSON.stringify(over(json), null, indentLevel) + endCharacters
+    if (hasWindowsNewlines) {
+      result = result.replace(/\n/g, '\r\n')
+    }
+    return result
   }
 
-  const prefixedScriptRegex = /^(pre|post)(.)/;
-  const prefixableScripts = defaultNpmScripts.slice();
-  if (typeof packageJson.scripts === 'object') {
-    Object.keys(packageJson.scripts).forEach(script => {
-      const prefixOmitted = script.replace(prefixedScriptRegex, '$2');
-      if (
-        packageJson.scripts[prefixOmitted] &&
-        !prefixableScripts.includes(prefixOmitted)
-      ) {
-        prefixableScripts.push(prefixOmitted);
-      }
-    });
-  }
+  return over(json)
+}
 
-  function sortSubKey(key, sortList, unique) {
-    if (Array.isArray(packageJson[key])) {
-      packageJson[key] = packageJson[key].sort();
-      if (unique) {
-        packageJson[key] = array_unique(packageJson[key]);
-      }
-      return;
+const prefixedScriptRegex = /^(pre|post)(.)/
+function toSortKey(prefixable) {
+  return function(script) {
+    const prefixOmitted = script.replace(prefixedScriptRegex, '$2')
+    if (prefixable.includes(prefixOmitted)) {
+      return prefixOmitted
     }
-    if (typeof packageJson[key] === 'object') {
-      packageJson[key] = sortObjectKeys(packageJson[key], sortList);
-    }
+    return script
   }
-  function toSortKey(script) {
-    const prefixOmitted = script.replace(prefixedScriptRegex, '$2');
-    if (prefixableScripts.includes(prefixOmitted)) {
-      return prefixOmitted;
-    }
-    return script;
-  }
-  /*             b
-   *       pre | * | post
-   *   pre  0  | - |  -
-   * a  *   +  | 0 |  -
-   *   post +  | + |  0
-   */
-  function compareScriptKeys(a, b) {
-    if (a === b) return 0;
-    const aScript = toSortKey(a);
-    const bScript = toSortKey(b);
+}
+
+/*             b
+ *       pre | * | post
+ *   pre  0  | - |  -
+ * a  *   +  | 0 |  -
+ *   post +  | + |  0
+ */
+function compareScriptKeys(sortKeyFn) {
+  return function(a, b) {
+    if (a === b) return 0
+    const aScript = sortKeyFn(a)
+    const bScript = sortKeyFn(b)
     if (aScript === bScript) {
       // pre* is always smaller; post* is always bigger
       // Covers: pre* vs. *; pre* vs. post*; * vs. post*
-      if (a === `pre${aScript}` || b === `post${bScript}`) return -1;
+      if (a === `pre${aScript}` || b === `post${bScript}`) return -1
       // The rest is bigger: * vs. *pre; *post vs. *pre; *post vs. *
-      return 1;
+      return 1
     }
-    return aScript < bScript ? -1 : 1;
+    return aScript < bScript ? -1 : 1
   }
-  function array_unique(array) {
-    return array.filter((el, index, arr) => index == arr.indexOf(el));
-  }
-  sortSubKey('keywords', null, true);
-  sortSubKey('homepage');
-  sortSubKey('bugs', ['url', 'email']);
-  sortSubKey('license', ['type', 'url']);
-  sortSubKey('author', ['name', 'email', 'url']);
-  sortSubKey('exports');
-  sortSubKey('bin');
-  sortSubKey('man');
-  sortSubKey('directories', ['lib', 'bin', 'man', 'doc', 'example']);
-  sortSubKey('repository', ['type', 'url']);
-  sortSubKey('funding', ['type', 'url']);
-  sortSubKey('scripts', compareScriptKeys);
-  sortSubKey('betterScripts', compareScriptKeys);
-  sortSubKey('commitlint');
-  sortSubKey('lint-staged');
-  sortSubKey('config');
-  sortSubKey('nodemonConfig');
-  sortSubKey('browserify');
-  sortSubKey('babel');
-  sortSubKey('eslintConfig');
-  sortSubKey('ava');
-  sortSubKey('jest');
-  sortSubKey('mocha');
-  sortSubKey('nyc');
-  sortSubKey('xo');
-  sortSubKey('prettier');
-  sortSubKey('dependencies');
-  sortSubKey('devDependencies');
-  sortSubKey('peerDependencies');
-  sortSubKey('bundledDependencies');
-  sortSubKey('bundleDependencies');
-  sortSubKey('optionalDependencies');
-  sortSubKey('resolutions');
-  sortSubKey('engines');
-  sortSubKey('engineStrict');
-  sortSubKey('os');
-  sortSubKey('cpu');
-  sortSubKey('preferGlobal');
-  sortSubKey('private');
-  sortSubKey('publishConfig');
-  packageJson = sortObjectKeys(packageJson, determinedSortOrder);
-  if (wasString) {
-    let result = JSON.stringify(packageJson, null, indentLevel) + endCharacters;
-    if (hasWindowsNewlines) {
-      result = result.replace(/\n/g, '\r\n');
-    }
-    return result;
-  }
-  return packageJson;
 }
-module.exports = sortPackageJson;
-module.exports.sortPackageJson = sortPackageJson;
-module.exports.sortOrder = sortOrder;
+
+function sortPackageJson(jsonIsh, options = {}) {
+  return editStringJSON(jsonIsh, json => {
+    const newJson = sortObjectKeys(options.sortOrder || sortOrder)(json)
+
+    for (const { key, over } of fields) {
+      if (over && newJson[key]) newJson[key] = over(newJson[key])
+    }
+
+    return newJson
+  })
+}
+
+module.exports = sortPackageJson
+module.exports.sortPackageJson = sortPackageJson
+module.exports.sortOrder = sortOrder
 
 if (require.main === module) {
-  const fs = require('fs');
-  const isCheckFlag = argument => argument === '--check' || argument === '-c';
+  const fs = require('fs')
+  const isCheckFlag = argument => argument === '--check' || argument === '-c'
 
-  const cliArguments = process.argv.slice(2);
-  const isCheck = cliArguments.some(isCheckFlag);
+  const cliArguments = process.argv.slice(2)
+  const isCheck = cliArguments.some(isCheckFlag)
 
-  const patterns = cliArguments.filter(argument => !isCheckFlag(argument));
+  const patterns = cliArguments.filter(argument => !isCheckFlag(argument))
 
   if (!patterns.length) {
-    patterns[0] = 'package.json';
+    patterns[0] = 'package.json'
   }
 
   const files = patterns.reduce(
-    (files, pattern) => files.concat(glob.sync(pattern)), 
-    []
-  );
+    (files, pattern) => files.concat(glob.sync(pattern)),
+    [],
+  )
 
-  let notSortedFiles = 0;
+  let notSortedFiles = 0
 
   files.forEach(file => {
-    const packageJson = fs.readFileSync(file, 'utf8');
-    const sorted = sortPackageJson(packageJson);
+    const packageJson = fs.readFileSync(file, 'utf8')
+    const sorted = sortPackageJson(packageJson)
 
     if (sorted !== packageJson) {
       if (isCheck) {
-        notSortedFiles ++;
-        console.log(file);
+        notSortedFiles++
+        console.log(file)
       } else {
-        fs.writeFileSync(file, sorted, 'utf8');
-        console.log(`${file} is sorted!`);
+        fs.writeFileSync(file, sorted, 'utf8')
+        console.log(`${file} is sorted!`)
       }
     }
-  });
+  })
 
   if (isCheck) {
     if (notSortedFiles) {
       console.log(
-        notSortedFiles === 1 ? 
-        `${notSortedFiles} file is not sorted.`:
-        `\n ${notSortedFiles} files are not sorted.`
-      );
+        notSortedFiles === 1
+          ? `${notSortedFiles} file is not sorted.`
+          : `\n ${notSortedFiles} files are not sorted.`,
+      )
     } else {
       console.log(
-        files.length === 1 ?
-        `file is sorted.`:
-        `all files are sorted.`
-      );
+        files.length === 1 ? `file is sorted.` : `all files are sorted.`,
+      )
     }
     process.exit(notSortedFiles)
   }
