64069133c4c479831a4dfc86ed3a029df6839a47
bluelovers <codelovers@users.sourceforge.net>
2020-01-19 04:34:15 +0800

    feat: add export default
    make sure import default can work

--- index.js
+++ index.js
@@ -324,3 +324,4 @@ function sortPackageJson(jsonIsh, options = {}) {
 module.exports = sortPackageJson
 module.exports.sortPackageJson = sortPackageJson
 module.exports.sortOrder = defaultSortOrder
+module.exports.default = sortPackageJson
