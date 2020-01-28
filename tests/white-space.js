abf99c9683d27664fc12cee3a9939d15c2e40588
fisker Cheung <lionkay@gmail.com>
2020-01-28 23:06:29 +0800

    chore: update `ava` to v3 (#153)
    * chore: update `ava` to v3
    
    * refactor: remove babel

--- tests/white-space.js
+++ tests/white-space.js
@@ -1,5 +1,5 @@
-import test from 'ava'
-import sortPackageJson from '..'
+const test = require('ava')
+const sortPackageJson = require('..')
 
 test('white space', t => {
   t.is(sortPackageJson('{}'), '{}')
