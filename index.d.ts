cfb6c8c161a8e854afeb297da8db27354835f6ed
sparebytes <sparebytes@gmail.com>
2020-05-23 13:21:32 -0500

    fix: typescript declarations
    - add override for string version of function
    - fix default export error in Typescript 3.9

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-/**
-   Sort packageJson.
+declare namespace sortPackageJsonExports {
+  interface SortPackageJsonFn {
+    /**
+      * Sort packageJson object.
+      *
+      * @param packageJson - A packageJson
+      * @param options
+      * @returns Sorted packageJson object
+      */
+    <T extends Record<any, any>>(packageJson: T, options?: Options): T,
 
-   @param packageJson - A packageJson object or string.
-   @param options
-   @returns Sorted packageJson object or string.
- */
-declare function sortPackageJsonCore<T extends Record<any, any>>(packageJson: T, options?: sortPackageJsonCore.Options): T;
-declare namespace sortPackageJsonCore {
-  var sortPackageJson: <T extends Record<any, any>>(packageJson: T, options?: sortPackageJsonCore.Options) => T;
-  // @ts-ignore
-  var default: <T extends Record<any, any>>(packageJson: T, options?: sortPackageJsonCore.Options) => T;
-}
-declare namespace sortPackageJsonCore {
+    /**
+      * Sort packageJson string.
+      *
+      * @param packageJson - A packageJson string.
+      * @param options
+      * @returns Sorted packageJson string.
+      */
+    (packageJson: string, options?: Options): string,
+  }
+
   type ComparatorFunction = (left: string, right: string) => number;
+
   interface Options {
     readonly sortOrder?: readonly string[] | ComparatorFunction;
   }
-  /**
-   Default sort order.
-   */
-  const sortOrder: readonly string[];
 }
-export = sortPackageJsonCore;
+
+interface sortPackageJsonExports extends sortPackageJsonExports.SortPackageJsonFn {
+  readonly default: sortPackageJsonExports.SortPackageJsonFn;
+  readonly sortPackageJson: sortPackageJsonExports.SortPackageJsonFn;
+}
+
+declare const sortPackageJsonExports: sortPackageJsonExports;
+
+export = sortPackageJsonExports;
