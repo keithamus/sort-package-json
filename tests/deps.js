3e4ff74ea4126a417c7e5aff6d6d9d8a4f7c8b8a
fisker Cheung <lionkay@gmail.com>
2022-04-04 22:30:51 +0800

    refactor!: migrate to esm and update dependencies (#256)
    * refactor!: migrate to esm
    
    * chore: remove unicorn
    
    * docs: update docs
    
    * ci: update npm
    
    * chore: update globby to v13
    
    * chore: update all deps
    
    * chore: upgrade all devdependencies
    
    Co-authored-by: Keith Cirkel <keithamus@users.noreply.github.com>

--- tests/deps.js
+++ tests/deps.js
@@ -1,5 +1,5 @@
-const test = require('ava')
-const { macro } = require('./_helpers')
+import test from 'ava'
+import { macro } from './_helpers.js'
 
 // `resolutions` and `dependencies`
 for (const field of [
