aa1a07e540269e12de429e597a3424652da57a3c
aarondill <aaronsacks2006@gmail.com>
2023-01-21 20:35:18 -0600

    refactor(cli): create output functions
    This commit creates stdout and stderr functions for outputting with
    respect to isQuiet and adds support for differing (easier to parse)
    messages if not connected to a TTY (eg. a pipe or process substitution)
    
    Additionally changes the 'No matching files.' message to output on stderr
    rather than on stdout as it was, which makes sense because this output
    represents erroneous usage.

--- cli.js
+++ cli.js
@@ -10,6 +10,26 @@ const cliArguments = process.argv.slice(2)
 const isCheck = cliArguments.some(isCheckFlag)
 const isQuiet = cliArguments.some(isQuietFlag)
 
+function stdout(outputIfTTY = '', alwaysOutput = outputIfTTY) {
+  if (isQuiet) return
+  const isTerminal = !!process.stdout.isTTY
+  if (isTerminal) {
+    console.log(outputIfTTY)
+  } else if (alwaysOutput !== null) {
+    console.log(alwaysOutput)
+  }
+}
+
+function stderr(outputIfTTY = '', alwaysOutput = outputIfTTY) {
+  if (isQuiet) return
+  const isTerminal = !!process.stdout.isTTY
+  if (isTerminal) {
+    console.error(outputIfTTY)
+  } else if (alwaysOutput !== null) {
+    console.error(alwaysOutput)
+  }
+}
+
 const patterns = cliArguments.filter((argument) => !isCheckFlag(argument))
 
 if (!patterns.length) {
@@ -19,11 +39,9 @@ if (!patterns.length) {
 const files = globbySync(patterns)
 
 if (files.length === 0) {
-  console.log('No matching files.')
+  stderr('No matching files.')
   process.exit(1)
 }
-
-if (isQuiet) console.log = function () {}
 
 let notSortedFiles = 0
 
@@ -34,24 +52,24 @@ files.forEach((file) => {
   if (sorted !== packageJson) {
     if (isCheck) {
       notSortedFiles++
-      console.log(file)
+      stdout(file)
     } else {
       fs.writeFileSync(file, sorted, 'utf8')
-      console.log(`${file} is sorted!`)
+      stdout(`${file} is sorted!`)
     }
   }
 })
 
 if (isCheck) {
-  console.log()
+  stdout()
   if (notSortedFiles) {
-    console.log(
+    stdout(
       notSortedFiles === 1
         ? `${notSortedFiles} of ${files.length} matched file is not sorted.`
         : `${notSortedFiles} of ${files.length} matched files are not sorted.`,
     )
   } else {
-    console.log(
+    stdout(
       files.length === 1
         ? `${files.length} matched file is sorted.`
         : `${files.length} matched files are sorted.`,
