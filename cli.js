6f1c5f490efd94b1f9ddfc3ea58236f878102e1d
aarondill <aaronsacks2006@gmail.com>
2023-01-22 19:28:04 -0600

    fix(cli): change stderr to not respect --quiet

--- cli.js
+++ cli.js
@@ -24,7 +24,6 @@ function stdout(outputIfTTY = '', alwaysOutput = outputIfTTY) {
 }
 
 function stderr(outputIfTTY = '', alwaysOutput = outputIfTTY) {
-  if (isQuiet) return
   const isTerminal = !!process.stdout.isTTY
   if (isTerminal) {
     console.error(outputIfTTY)
