f268402b936e5f7e95bdcd452e24717252b80c58
Aaron Dill <117116764+aarondill@users.noreply.github.com>
2023-01-30 04:31:14 -0600

    feat(cli): add --quiet / -q option (#281)
    * feat(cli): add --quiet / -q option
    
    Stops any successful output. Errors are still outputted.
    
    * refactor(cli): create output functions
    
    This commit creates stdout and stderr functions for outputting with
    respect to isQuiet and adds support for differing (easier to parse)
    messages if not connected to a TTY (eg. a pipe or process substitution)
    
    Additionally changes the 'No matching files.' message to output on stderr
    rather than on stdout as it was, which makes sense because this output
    represents erroneous usage.
    
    * fix(cli): ensure --quiet isn't used as a pattern
    
    --help and --version don't need to be checked because they
    exit the program before reaching this point.
    
    * fix(cli): add --quiet to the help
    
    * fix(cli): change stderr to not respect --quiet
    
    * fix(cli): change stdout to stderr in isTerminal
    
    * refactor(cli): simplify output functions
    
    * refactor(cli): remove call to array.filter
    
    * test: add tests for version, help, and quiet

--- cli.js
+++ cli.js
@@ -7,9 +7,15 @@ const isCheckFlag = (argument) => argument === '--check' || argument === '-c'
 const isHelpFlag = (argument) => argument === '--help' || argument === '-h'
 const isVersionFlag = (argument) =>
   argument === '--version' || argument === '-v'
+const isQuietFlag = (argument) => argument === '--quiet' || argument === '-q'
 
 const cliArguments = process.argv.slice(2)
 const isCheck = cliArguments.some(isCheckFlag)
+const isQuiet = cliArguments.some(isQuietFlag)
+
+const stdout = isQuiet ? () => {} : console.log
+const stderr = console.error
+
 const isHelp = cliArguments.some(isHelpFlag)
 const isVersion = cliArguments.some(isVersionFlag)
 
@@ -20,6 +26,7 @@ Sort npm package.json files. Default: ./package.json
 Strings passed as files are parsed as globs.
 
   -c, --check                check if FILES are sorted
+  -q, --quiet                don't output success messages
   -h, --help                 display this help and exit
   -v, --version              display the version and exit
   `,
@@ -35,7 +42,9 @@ if (isVersion) {
   process.exit(0)
 }
 
-const patterns = cliArguments.filter((argument) => !isCheckFlag(argument))
+const patterns = cliArguments.filter(
+  (argument) => !isCheckFlag(argument) && !isQuietFlag(argument),
+)
 
 if (!patterns.length) {
   patterns[0] = 'package.json'
@@ -44,7 +53,7 @@ if (!patterns.length) {
 const files = globbySync(patterns)
 
 if (files.length === 0) {
-  console.log('No matching files.')
+  stderr('No matching files.')
   process.exit(1)
 }
 
@@ -57,24 +66,24 @@ files.forEach((file) => {
   if (sorted !== packageJson) {
     if (isCheck) {
       notSortedFiles++
-      console.log(file)
+      stdout(file)
     } else {
       fs.writeFileSync(file, sorted, 'utf8')
-      console.log(`${file} is sorted!`)
+      stdout(`${file} is sorted!`)
     }
   }
 })
 
 if (isCheck) {
-  console.log()
+  stdout()
   if (notSortedFiles) {
-    console.log(
+    stdout(
       notSortedFiles === 1
         ? `${notSortedFiles} of ${files.length} matched file is not sorted.`
         : `${notSortedFiles} of ${files.length} matched files are not sorted.`,
     )
   } else {
-    console.log(
+    stdout(
       files.length === 1
         ? `${files.length} matched file is sorted.`
         : `${files.length} matched files are sorted.`,
